374f72b5d060181e27b3b0b030901726a5682103
MateuszDlu <mateuszdluzniewski02@gmail.com>
2024-07-25 16:41:16 +0200

    first prototype of scheduler content, need real data

--- ssscheduler_frontend/src/pages/SchedulerPage.tsx
+++ ssscheduler_frontend/src/pages/SchedulerPage.tsx
@@ -1,15 +1,30 @@
+import { useTranslation } from "react-i18next";
+import { json, useNavigate } from "react-router-dom";
+import { FunctionComponent, useCallback, useState } from "react";
+import axios from "axios";
+import { LOCAL_HOST_API_URL } from "utilities/AppConstants";
+import "../styles/schedulerPage.css"
 
 
 const SchedulerPage = () => {
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+    const [notificationMessage, setNotificationMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
 
     return(
         <>
         <body>
             <head>
-                <title></title>
+                <title>{t('scheduler.headTitle')}</title>
             </head>
-            <div className="categoryContainer">
-                <h1>hello there</h1>
+            <div className="schedulerContainer">
+                <div className="categoryContainer"><h1>Category 1</h1></div>
+                <div className="categoryContainer"><h1>Category 2</h1></div>
+                <div className="categoryContainer"><h1>Category 3</h1></div>
+                <div className="categoryContainer"><h1>Category 4</h1></div>
+                <div className="categoryContainer"><h1>Category 5</h1></div>
             </div>
         </body>
         </>
